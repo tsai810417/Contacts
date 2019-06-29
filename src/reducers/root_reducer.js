cfb96869eb8cc1e1b87e392e8000a9e20200abfe
Chris Tsai <tsai_cheng@yahoo.com>
2019-06-29 12:03:17 +0800

    fix typo

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import contactReducer from './contacts_reducer';
+import contactReducer from './contact_reducer';
 
 const rootReducer = combineReducers({
   contacts: contactReducer
