84840cd465df1597177cea399bd4fc84010f7ec5
Chris Tsai <tsai_cheng@yahoo.com>
2019-06-29 21:30:10 +0800

    test actions in console

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 import configureStore from './store/store';
+// for test start
+import {
+  insertContact,
+  updateContact,
+  removeContact
+} from './actions/contact_actions';
+// for test end
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore({});
+  // for test start
+  window.store = store;
+  window.insertContact = insertContact;
+  window.updateContact = updateContact;
+  window.removeContact = removeContact
+  // for test end
 
   ReactDOM.render(<App store={ store }/>, document.getElementById('root'));
 });
